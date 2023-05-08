eda9782d798a191eb1c0f508a407cfcb6237d31e
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-05-08 13:16:37 +0300

    Create base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    Base class for other classes in the project.
+    """
+
+    def __init__(self):
+        """
+        Initializes a BaseModel instance.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """
+        Returns a string representation of the BaseModel instance.
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the public instance attribute 'updated_at' with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+        """
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
