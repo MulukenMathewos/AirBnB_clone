765ae0bd57de059d5d80895de92dc816cae8cb13
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-25 04:46:10 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -85,9 +85,7 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help(self):
-        h = ("Documented commands (type help ):"
-             "========================================"
-             "EOF  all  count  create  destroy  help  quit  show  update")
+        h = "Documented commands (type help):\n========================================\nEOF  all  count  create  destroy  help  quit  show  update"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help"))
             self.assertEqual(h, output.getvalue().strip())
