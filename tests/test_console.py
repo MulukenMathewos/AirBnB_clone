3b63cf9158dd2ce2037759ec207a5fadd6d42067
Muluken Mathewos <93589524+MulukenMathewos@users.noreply.github.com>
2023-07-25 04:51:44 +0300

    Update

--- tests/test_console.py
+++ tests/test_console.py
@@ -85,7 +85,9 @@ class TestHBNBCommand_help(unittest.TestCase):
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help(self):
-        h = "Documented commands (type help):\n========================================\nEOF  all  count  create  destroy  help  quit  show  update"
+        h = "Documented commands (type help):\n" \
+        "========================================\n" \
+        "EOF  all  count  create  destroy  help  quit  show  update"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help"))
             self.assertEqual(h, output.getvalue().strip())
@@ -142,6 +144,7 @@ class TestHBNBCommand_create(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("MyModel.create()"))
             self.assertEqual(correct, output.getvalue().strip())
+        correct = "*** Unknown syntax: BaseModel.create()"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("BaseModel.create()"))
             self.assertEqual(correct, output.getvalue().strip())
